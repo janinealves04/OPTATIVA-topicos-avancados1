7ea29772d7d94f5cdba16f0e6e142ee49d2c6ea6
Janine Alves da Silva <20221148060025@ifrn.local>
2024-08-30 17:37:40 -0300

    Corrigindo DatasetController reorganizando  a validação dos dados da requisição e criando transação para posteriormente conectar-se ao AMQP.

--- datasets-backend/src/controller/DatasetController.js
+++ datasets-backend/src/controller/DatasetController.js
@@ -7,6 +7,7 @@ import Datasets from "../models/Dataset.js";
 import { StatusCodes } from "http-status-codes";
 import { datasetSchemaValidate } from "../utils/validateControllers.js";
 import connectAMQP from "../config/connectAMQP.js";
+import mongoose from "mongoose";
 
 
 /**
@@ -17,51 +18,33 @@ import connectAMQP from "../config/connectAMQP.js";
  * @returns {void} - Não retorna um valor, mas envia uma resposta HTTP.
  */
 export async function createDataset(req, res) {
+    
+    const session = await mongoose.startSession();
+    session.startTransaction(); 
+      
     try {
         
         const validatedData = await datasetSchemaValidate.validate(req.body, {abortEarly : false});
-        
         const filePath = req.file ? req.file.path : null;
 
-        if (filePath === null) {
-            return res.status(StatusCodes.BAD_REQUEST).send({ message: "File not send" });
-        }
-  
         //Database
         const {name, description} = validatedData;
         const dataset = new Datasets({ name, description, filePath });
         await dataset.save();
+        await Datasets.create([{ name, description, filePath}, {session}])
 
         //AMQP
         const channel = await connectAMQP();
-        if (channel){
-            const message = JSON.stringify({name, filePath});
-            channel.sendToQueue("datasets", Buffer.from(message)); //Transform in bytes
-            console.log(`Send message to amqp: ${message}`);
-        }else{
             throw new Error("Failed to connect AMQP");
-        }    
+            
 
-
+        //Commit session 
+        await session.commitTransaction();
 
         res.status(StatusCodes.CREATED).send();
     } catch (error) {
+        await session.abortTransaction();
         res.status(StatusCodes.BAD_REQUEST).send(error);
     }
-}
-
-/**
- * Recupera todos os datasets armazenados no banco de dados.
- *
- * @param {Object} req - O objeto de requisição do Express.
- * @param {Object} res - O objeto de resposta do Express, usado para enviar os dados dos datasets ao cliente.
- * @returns {void} - Não retorna um valor, mas envia uma resposta HTTP contendo os datasets ou um erro.
- */
-export async function getDatasets(req, res) {
-    try {
-        const datasets = await Datasets.find({});
-        res.send(datasets);
-    } catch (error) {
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(error);
-    }
+    session.endSession();
 }
